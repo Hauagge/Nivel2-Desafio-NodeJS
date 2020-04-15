5e12a3096893293e5fff5f808ce0d6e1d1083bf9
Gabriel Hauagge <gabrielhauagge@hotmail.com>
2020-04-15 13:09:02 -0300

    finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const prevIncome = this.transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        return transaction.value;
+      }
+      return 0;
+    });
+    const prevOutcome = this.transactions.map(transaction => {
+      if (transaction.type === 'outcome') {
+        return transaction.value;
+      }
+      return 0;
+    });
+    const income = prevIncome.reduce((sum, value) => {
+      return sum + value;
+    }, 0);
+
+    const outcome = prevOutcome.reduce((sum, value) => {
+      return sum + value;
+    }, 0);
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
